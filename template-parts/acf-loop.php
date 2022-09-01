94e987a593a04f7b723c277ccd24745047c61412
Ryan Anderson <54652404+ryanaanderson0@users.noreply.github.com>
2022-08-31 23:06:33 -0700

    added font and hero image div overlay

--- template-parts/acf-loop.php
+++ template-parts/acf-loop.php
@@ -3,10 +3,12 @@
 			<?php while( have_rows('page_modules') ): the_row(); ?>
 
 				<?php if( get_row_layout() == 'hero_image_module' ): ?> <!-- Hero Image Module -->
-                    <div class="container-fluid p-0">
-                        <figure class="hero-image">
-                             <img src="<?php the_sub_field('hero_image_module_image') ?>">
-                        </figure>
+                    <div class="container-fluid hero-image p-0 d-flex " style="background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url('<?php the_sub_field('hero_image_module_image') ?> '); background-repeat: none; background-position: center; background-size: cover;">
+                        <div class="hero-text text-center shadow-lg">
+                            <h1>Fresh Persimmon</h1>
+                            <p class="py-3">A good persimmon at its peak will taste sweet, mild, and rich. </br>Many people have described its flavor as “honey-like.”</p>
+                            <button class="primary-button">Eat me</button>
+                        </div>
                     </div>
 
 
